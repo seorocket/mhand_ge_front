3617096680fcf3792ec6b8da69b74dfdfe7e7b0c
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2023-04-10 13:32:36 +0400

    upd

--- scripts/modules/untils.js
+++ scripts/modules/untils.js
@@ -13,3 +13,28 @@ $('.all-calendar').on('click', function () {
     $(elements[key]).removeClass('open')
   })
 })
+
+$(function() {
+	var tab = $('#tabs .tabs-items > div'); 
+	tab.hide().filter(':first').show(); 
+	
+	// Клики по вкладкам.
+	$('#tabs .tabs-nav a').click(function(){
+		tab.hide(); 
+		tab.filter(this.hash).show(); 
+		$('#tabs .tabs-nav a').removeClass('active');
+		$(this).addClass('active');
+		return false;
+	}).filter(':first').click();
+ 
+	// Клики по якорным ссылкам.
+	$('.tabs-target').click(function(){
+		$('#tabs .tabs-nav a[href=' + $(this).attr('href')+ ']').click();
+	});
+	
+	// Отрытие вкладки из хеша URL
+	if(window.location.hash){
+		$('#tabs-nav a[href=' + window.location.hash + ']').click();
+		window.scrollTo(0, $("#" . window.location.hash).offset().top);
+	}
+});
